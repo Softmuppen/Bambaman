dbb9f9da2930e344eb09e49d953f1d5e2a345f32
Softmuppen <rich.norling@gmail.com>
2013-05-14 21:07:36 +0200

    CHARACTER UPDATE!

---
+++ src/Character/Player.java
@@ -0,0 +1,173 @@
+package Character;
+
+import Utility.Entity;
+
+public class Player extends Entity {
+
+	private String name;
+	private String direction;
+	private int health;
+	private int dx;
+	private int dy;
+
+	public Player(String name, int health){
+		super(id, x, y, width, height);  
+		this.health = health;
+		direction = "right";
+		dx = dy = 0;
+
+	}
+	
+    public void move()
+	{
+    	// Move character
+    	setY(getY()+getDy());
+        setX(getX()+getDx());
+
+        
+        // Set the current direction
+        if(getDx() > 0){
+        	setDirection("right");
+        }else if(getDx() < 0) {
+        	setDirection("left");
+        }
+        
+        if(getDy() > 0){
+        	setDirection("down");
+        }else if(getDy() < 0){
+        	setDirection("up");
+        }
+	}
+    
+    /**
+     * Moves the player and updates its attacking status.
+     */
+    public void update(){
+        move();
+    }
+    
+    /**
+	 * Returns the max health this player can have.
+	 * @return The max capacity for player's health.
+	 */
+	public int getMaxHealth(){
+		return maxHealth;
+	}
+	     
+	/**
+	 * Sets a new value for maxHealth.
+	 * @param maxHealth The new value for max capacity health.
+	 */
+	public void setMaxHealth(int maxHealth){
+		this.maxHealth = maxHealth;
+	}
+	
+	   /**
+	    * Returns the latest direction of this character.
+	    * @return direction The current direction this Character is facing.
+	    */
+	   public String getDirection()
+	   {
+	       return direction;
+	   }  
+	   
+	   /**
+	    * Sets the current direction of this character
+	    * @param arg The new value for direction.
+	    */
+	   public void setDirection(String arg)
+	   {
+	       direction = arg;
+	   } 
+	   
+	   public void setDx(int dx)
+	   {
+		   this.dx = dx;
+	   }
+	   public void setDy(int dy)
+	   {
+		   this.dy = dy;
+	   }
+	   
+	   /**
+	    * Returns the current argument for dx.
+	    * @return Argument for dx.
+	    */
+	   public int getDx()
+	   {
+		   return dx;
+	   }
+	   
+	   /**
+	    * Returns the current argument for dy.
+	    * @return Argument for dy.
+	    */
+	   public int getDy()
+	   {
+		   return dy;
+	   }
+	   
+	   /**
+	    * Reset direction of this Character.
+	    */
+	   public void resetDirection()
+	   {
+		   dx = 0;
+		   dy = 0;
+	   }
+	   
+	   /**
+	    * Equals-method used to compare two characters.
+	    * @return Boolean showing if the two characters were the same or not.
+	    */
+	   @Override
+	   public boolean equals(Object obj)
+	   {
+		   /*if( this.getY() == ((Character) obj).getY() && this.getX() == ((Character) obj).getX() ){
+			   return(true);
+		   }*/
+		   
+		   if( this.getName() == ((Character)obj).getName()){
+			   return true;
+		   }
+		   
+		   return(false);
+	   }
+	   
+	   /**
+	    * Returns the health of this Character.
+	    * @return The current health of this Character.
+	    */
+	   public int getHealth()
+	   {
+	       return health;
+	   }
+	   
+	   /**
+	    * Sets a new value for health.
+	    * @param health The new value for health.
+	    */
+	   public void setHealth(int health)
+	   {
+		   this.health = health;
+		   setChanged();
+		   notifyObservers("information");
+	   }  
+	   
+	   /**
+	    * Returns the name of this Character.
+	    * @return The name of this Character.
+	    */
+	   public String getName()
+	   {
+	       return name;
+	   }
+	   
+	   /**
+	    * Sets the character name
+	    */
+	   public void setName(String name){
+		   this.name = name;
+	   }
+
+}
